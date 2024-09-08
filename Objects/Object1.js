e911e0e5d80fb3677d26b1b544fa3e6faaecb330
Mahigavale <Maheshgavale07@gmail.com>
2024-09-08 08:09:33 +0530

    js Objects

---
+++ Objects/Object1.js
@@ -0,0 +1,19 @@
+
+
+const employee={
+    name:"Mahesh",
+    age:"23",
+    profession:"Unemployed",
+    state:"maharashtra",
+    getage:function()
+    {
+        return this.name+ " "+this.age;
+    }
+}
+
+console.log(employee);
+
+console.log(employee.getage());
+console.log(employee.name);
+
+
