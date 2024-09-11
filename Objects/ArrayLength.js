ef1fd6d75d8321faf38a7a978f0ff048092a7664
Mahigavale <Maheshgavale07@gmail.com>
2024-09-11 15:01:33 +0530

    JS

--- Objects/ArrayLength.js
+++ Objects/ArrayLength.js
@@ -10,6 +10,15 @@ console.log(arrays);
 console.log(arrays.join("*"));
 
 console.log(arrays.toString());
+//toString => converts to String with the seperator of Comma.
+//join=> converts the array to String with the help of the seperator.
+
+
+let array2=[12,24,45,67]
+
+console.log(arrays.concat(array2));
+//concat => dosen't modifies the original array.
+
 
 console.log(arrays);
 
